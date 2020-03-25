bcbc01a2bda813152f2d5b18bd179ad63e3b117c
Ninoslav Ciric <ninoslav.ciric89@gmail.com>
2020-03-25 14:36:37 +0100

    minor fix

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-const nesto = document.querySelectorAll('.one-field');
+const nesto = document.querySelectorAll('.one-field'); //sa svim elementima radim
 let kontrolaZaPotez
 
-nesto.forEach(function(element) {
+nesto.forEach(function(element) { // automatski ga pretvara u array
     element.addEventListener('click', function (){
     if (!kontrolaZaPotez){
         let iks = document.createElement('a'); // NE MOZE VAN OVE NAPRAVITI ELEMENT FUNKCIJE INACE CE IH BRISATI SVAKIM KLIKOM
@@ -22,7 +22,7 @@ nesto.forEach(function(element) {
         kontrolaZaPotez = !kontrolaZaPotez;
         }
     
-    if(winner1()){
+    if(winner1()){ //Provera ko je pobednik, ovde cu dodati kasnije brojace za elemente dole koji ce da upisuju score, kao i local storage
         console.log('x wins');
         alert('X wins');
     }
@@ -36,9 +36,8 @@ nesto.forEach(function(element) {
     }
     }, { once: true} );
   });
-
-const provera = document.getElementsByClassName('eks')
-const winningCombination = [
+  
+const winningCombination = [ // jedine moguce kombinacije
 [0, 1, 2],
 [3, 4, 5],
 [6, 7, 8],
@@ -49,26 +48,28 @@ const winningCombination = [
 [2, 4, 6]
 ];
 
-function winner1() {
+function winner1() { //provera za x
 return winningCombination.some(combination => {
     return combination.every(index => {
     return nesto[index].classList.contains('x')
     })
 })
 }
-function winner2() {
+function winner2() { // provera za o
 return winningCombination.some(combination1 => {
     return combination1.every(index1 => {
     return nesto[index1].classList.contains('o')
     })
 })
 }
-function tigh() {
+function tigh() { // ako su sva polja ispunjena da bude nereseno
 return [...nesto].every(cell => {
     return cell.classList.contains('x') || cell.classList.contains('o')
     })
 }
-let anchorReset = document.getElementById('reset');
+
+
+let anchorReset = document.getElementById('reset'); // resetovanje table...jel moze ovako?
 anchorReset.addEventListener('click', function(){
 window.location.reload(false);
 })
